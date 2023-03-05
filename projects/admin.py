d6fcb3bd47ad8e4a0c6435cb08b04a72c05c3e9f
derishev <derishev.aleks@yandex.ru>
2023-03-05 17:44:25 +0300

    lesson_6 finish

--- projects/admin.py
+++ projects/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from .models import Project, ToDo
 
 
 @admin.register(Project)
-class CustomUserAdmin(UserAdmin):
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('email',)}),
-    )   # Expanding the list of fields to display in the form
+class ProjectAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'href',)
     list_display_links = ('id', 'name',)
     search_fields = ('name',)
